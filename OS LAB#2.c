66cd0ac829d92454f84791f81ff198aecadffb72
Shahmeer-Khan99 <79381378+Shahmeer-Khan99@users.noreply.github.com>
2021-04-13 09:15:54 +0500

    Add files via upload

---
+++ OS LAB#2.c
@@ -0,0 +1,141 @@
+#include  <stdio.h>
+#include  <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_INPUT_SIZE 1024
+#define MAX_TOKEN_SIZE 64
+#define MAX_NUM_TOKENS 64
+
+/* Splits the string by space and returns the array of tokens
+*
+*/
+
+// Function where the system command is executed
+void execArgs(char** parsed)
+{
+    // Forking a child
+    pid_t pid = fork(); 
+  
+    if (pid == -1) {
+        printf("\nFailed forking child..");
+        return;
+    } else if (pid == 0) {
+        if (execvp(parsed[0], parsed) < 0) {
+            printf("\nCould not execute command..");
+        }
+        exit(0);
+    } else {
+        // waiting for child to terminate
+        wait(NULL); 
+        return;
+    }
+}
+
+
+
+char **tokenize(char *line)
+{
+  char **tokens = (char **)malloc(MAX_NUM_TOKENS * sizeof(char *));
+  char *token = (char *)malloc(MAX_TOKEN_SIZE * sizeof(char));
+  int i, tokenIndex = 0, tokenNo = 0;
+
+  for(i =0; i < strlen(line); i++){
+
+    char readChar = line[i];
+
+    if (readChar == ' ' || readChar == '\n' || readChar == '\t'){
+      token[tokenIndex] = '\0';
+      if (tokenIndex != 0){
+	tokens[tokenNo] = (char*)malloc(MAX_TOKEN_SIZE*sizeof(char));
+	strcpy(tokens[tokenNo++], token);
+	tokenIndex = 0; 
+      }
+    } else {
+      token[tokenIndex++] = readChar;
+    }
+  }
+ 
+  free(token);
+  tokens[tokenNo] = NULL ;
+  return tokens;
+}
+
+
+int main(int argc, char* argv[]) {
+	char  line[MAX_INPUT_SIZE];            
+	char  **tokens;              
+	int i;
+
+	FILE* fp;
+	if(argc == 2) {
+		fp = fopen(argv[1],"r");
+		if(fp < 0) {
+			printf("File doesn't exists.");
+			return -1;
+		}
+	}
+
+	while(1) {			
+		/* BEGIN: TAKING INPUT */
+		bzero(line, sizeof(line));
+		if(argc == 2) { // batch mode
+			if(fgets(line, sizeof(line), fp) == NULL) { // file reading finished
+				break;	
+			}
+			line[strlen(line) - 1] = '\0';
+		} else { // interactive mode
+			printf("$ ");
+			scanf("%[^\n]", line);
+			getchar();
+		}
+		printf("Command entered: %s (remove this debug output later)\n", line);
+		/* END: TAKING INPUT */
+
+		line[strlen(line)] = '\n'; //terminate with new line
+		tokens = tokenize(line);
+		int size_tn;
+		size_tn = sizeof(tokens)/sizeof(tokens[0]);
+		printf("\n Len of token is : %d" , size_tn);
+		
+		
+		if(strcmp(tokens[0] , "cd") == 0 ) {
+			if(tokens[1] != NULL && tokens[1] != ".."){
+				
+				chdir(tokens[1]);
+			}
+			else if(tokens[1] == NULL){
+				chdir("HOME");
+			}
+			
+			else if (tokens[1] == ".."){
+				chdir("..");
+			}
+		}
+		
+		else {
+			execArgs(tokens);
+		}
+		
+		
+		
+		
+		
+   
+       //do whatever you want with the commands, here we just print them
+
+		for(i=0;tokens[i]!=NULL;i++){
+			printf("found token %s (remove this debug output later)\n", tokens[i]);
+		}
+       
+		// Freeing the allocated memory	
+		for(i=0;tokens[i]!=NULL;i++){
+			free(tokens[i]);
+		}
+		free(tokens);
+
+	}
+	return 0;
+}
